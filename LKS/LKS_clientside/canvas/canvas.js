7da007f55110c456709658048519f18a79f2d603
Robby Zhang <43869897+Robbyzhg@users.noreply.github.com>
2019-10-27 07:03:32 -0700

    Add files via upload

---
+++ LKS/LKS_clientside/canvas/canvas.js
@@ -0,0 +1,19 @@
+//mengambil id canvas
+var canvas = document.getElementById("canvas");
+//membuat context 2d
+var ctx = canvas.getContext("2d");
+
+//membuat garis x,y
+ctx.fillRect(10,10,15,15);
+ctx.strokeRect(25,10,15,15);
+ctx.fillRect(40,10,15,15);
+ctx.strokeRect(55,10,15,15);
+
+ctx.fillStyle("black");
+
+
+
+
+
+
+
